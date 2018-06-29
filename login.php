dc41360f0d42291518034f4168ac7764a0ce075b
Rong <rzheng@fau.edu>
2018-06-28 22:24:15 -0400

    Add files via upload

---
+++ login.php
@@ -0,0 +1,16 @@
+<html>
+<body>
+<div>
+<p>Login</p>
+<form name ="form1" method="post" action="LoginC.php">
+<p>Username: <input type="text" name="username" size="15" maxlength="20" value = ""></p>
+<p>Password: <input type="password" name="password" size="15" maxlength="20" value = ""></p>
+
+<p><input type="submit" name="submit" value="Login"></p>
+
+
+
+</form>
+</div>
+</body>
+</html>
