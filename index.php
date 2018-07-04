4d092a15b78d664dd6a91808d59d5af5a25ec788
Rong <rzheng@fau.edu>
2018-07-04 01:56:20 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+class Message{
+	var $name;
+	var $time;
+	var $content;
+
+	function __construct($n, $t, $c)
+	{
+	$this->name = $n;
+	$this->time = $t;
+	$this->content = $c;
+	}
+
+	function show()
+	{
+	echo "Name: " .$this->name."<br>";
+	echo "Time: " .$this->time."<br>";
+	echo "Message: " .$this->content."<br>";
+	echo "=======================================<br>";
+	}
+}
+
+//$m = new Message("Rong","2018-06-20", "I am Rong.");
+//echo $m->show();
+
+//var_dump($m);
+?>
--- message.php
+++ message.php
@@ -1,27 +1,46 @@
 <?php
-class Message{
-	var $name;
-	var $time;
-	var $content;
+include_once('index.php');
+class MessageBoard{
+	var $message = array();
 
-	function __construct($n, $t, $c)
-	{
-	$this->name = $n;
-	$this->time = $t;
-	$this->content = $c;
-	}
-
-	function show()
-	{
-	echo "Name: " .$this->name."<br>";
-	echo "Time: " .$this->time."<br>";
-	echo "Message: " .$this->content."<br>";
-	echo "=======================================";
-	}
+	function __construct(){
+		$this->receivem();
+		$this->loadm();
+		$this->showam();
+		$this->showform();
+}
+	function receivem(){
+		if(count($_POST) != 0)
+		{
+			$this->savem($_POST['username'], date("Y-m-d h:i:s", time()), $_POST['content']);
+		}
+}
+	function savem($n, $t, $c){
+		echo "Username: ".$n."<br>";
+		echo "time: ".$t."<br>";
+		echo "Message: ".$c."<br>";
+}
+	function loadm(){
+		$temp = new message("rong", "2018-06-21","pinapple pinapple pinapple.");
+		array_push($this->message, $temp);
+		$temp = new message("KK", "2018-06-23", "I am Kk.");
+		array_push($this->message, $temp);
+}
+	function showam(){
+		foreach($this->message as $m){
+			$m->show();
+}
+}
+	function showform(){
+		echo "<form action='' method='POST' >";
+		echo "Name: "."<input type='text' name='username'><br>";
+		echo "Content: "."<input type='text' name='content'><br>";
+		echo "<input type='submit' name='submit' value='Post Message'>";
+		echo "</form>";
+}	
 }
 
-$m = new Message("Rong","2018-06-20", "I am Rong.");
-echo $m->show();
+//implement
+$mb=new MessageBoard();
 
-//var_dump($m);
 ?>
