6115ee21149acc60e1563212520f848e7baba6ca
Rong <rzheng@fau.edu>
2018-07-03 21:55:52 -0400

    Add files via upload

--- LoginC.php
+++ LoginC.php
@@ -3,6 +3,13 @@ include('connect.php');
 $test=$_POST['username'];
 $test1=$_POST['password'];
 
+if($test == "" || $test1 == "")
+{
+echo "Username and password cannot be empty";
+echo "<br><a href=https://lamp.cse.fau.edu/~rzheng/login.php>Go Back</a>";
+echo "<br><a href=https://lamp.cse.fau.edu/~rzheng/test.php>Register</a>";
+}
+
 if(ctype_alnum($test)){ 
 //echo "<br>username accept";
 //check to see if username in use
@@ -13,6 +20,7 @@ $num=mysqli_num_rows($result);
 if ($num != 0)
 {
 $row = $result->fetch_assoc();
+
 if ($row["password"] == $test1)
 {
 echo "You are connected";
@@ -20,12 +28,17 @@ echo "You are connected";
 else
 {
 echo "Sorry, password not correct.";
-
 echo "<br><a href=https://lamp.cse.fau.edu/~rzheng/login.php>Go Back</a>";
 echo "<br><a href=https://lamp.cse.fau.edu/~rzheng/test.php>Register</a>";
 //echo "<br><a href=https://lamp.cse.fau.edu/~rzheng/.php>Change Password</a>";
+}
 
 }
+else
+{
+echo "No user found";
+echo "<br><a href=https://lamp.cse.fau.edu/~rzheng/login.php>Go Back</a>";
+echo "<br><a href=https://lamp.cse.fau.edu/~rzheng/test.php>Register</a>";
 }
 }
 
--- login.php
+++ login.php
@@ -7,10 +7,9 @@
 <p>Password: <input type="password" name="password" size="15" maxlength="20" value = ""></p>
 
 <p><input type="submit" name="submit" value="Login"></p>
-
-
-
 </form>
+<p>New user? <a href="https://lamp.cse.fau.edu/~rzheng/test.php" class="button">Register</a></p>
+
 </div>
 </body>
 </html>
